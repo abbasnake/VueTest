c885e556084f0c38a886a64917fd147d2bf6ea74
abbasnake <mr.rihards.ozols@gmail.com>
2018-04-10 09:39:28 +0300

    changed layout configuration

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="l-app">
-    <router-view/>
+    <router-view class="l-app-view"/>
   </div>
 </template>
 
@@ -10,7 +10,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 html, body, h1, h2, h3, h4, h5, h6 {
   box-sizing: border-box;
   margin: 0;
@@ -18,6 +18,14 @@ html, body, h1, h2, h3, h4, h5, h6 {
 }
 
 .l-app {
+  display: grid;
   height: 100vh;
+  grid-template-columns: 1fr 2fr 2fr 1fr;
+  grid-template-rows: 1fr 2fr 2fr 1fr;
+  &-view {
+    border: 2px solid black;
+    grid-column: 2/4;
+    grid-row: 2/3;
+  }
 }
 </style>
